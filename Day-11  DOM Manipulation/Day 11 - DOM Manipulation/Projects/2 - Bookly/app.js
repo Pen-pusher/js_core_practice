bf56187515d9e2598e966ffc0d71da1621230ac2
klassynihal <harshaan.n.khan@gmail.com>
2018-10-24 17:15:17 +0530

    Bookly complete

--- Day-11  DOM Manipulation/Day 11 - DOM Manipulation/Projects/2 - Bookly/app.js
+++ Day-11  DOM Manipulation/Day 11 - DOM Manipulation/Projects/2 - Bookly/app.js
@@ -2,70 +2,81 @@ let btn = document.getElementById('addBtn');
 let input = document.querySelector('#add-book input[type=text]');
 let renderBox = document.querySelector('#book-list ul');
 let hideChkBox = document.getElementById('hide');
+let srchFltr = document.querySelector('#search-books input');
 var bookList = JSON.parse(localStorage.getItem('bookListData')) || [];
+var htmlList = [];
+
+//-------------GENERAL FUNCTION------------
+function updtStrg() {
+  localStorage.setItem('bookListData', JSON.stringify(bookList));
+  cnvrtToHTML();
+}
+
+function cnvrtToHTML() {
+  htmlList = bookList.map((v, i) => {
+    return (
+      `<li>
+        <span data-id="${i}" class="bookName"> ${v} </span>
+        <button data-id="${i}" class="delete"> del </button>
+      </li>`
+    )
+  });
+}
+
+updtStrg();
+rndrBooks(htmlList);
+
+
 
 // // -----------------DISPLAY FUNCTION-----------
-function rndrBooks() {
-  // renderBox.innerHTML = '';
-  bookList.forEach((v, i) => {
-    let renderStr = (`<p data-id = "${i}" class = "bookName"> ${v}</p> <button class="delBtn">del</button>`);
-    let li = document.createElement("LI");
-    li.innerHTML = renderStr;
-    console.log(li);
-    renderBox.appendChild(li);
-    console.log(renderBox);
-    // return;
+function rndrBooks(booksArr = []) {
+  renderBox.innerHTML = '';
+  booksArr.forEach( v => {
+    renderBox.innerHTML += v;
   });
-  rndrBooks();
-}
-function updtStrg() {
-  localStorage.setItem('bookListData', JSON.stringify (bookList));
+
 }
 
 // // ------ADD TO ARRAY FUNCTION---------
-function addBook() {
-  // if (input.value) {
-    // bookList.push(input.value);
-    // updtStrg();
-    rndrBooks();
-  // }
-  // input.value = '';
-  // return;
+function addBook(e) {
+  e.preventDefault();
+  if (input.value) {
+    bookList.push(input.value);
+    updtStrg();
+    rndrBooks(htmlList);
+  }
+  input.value = '';
+  return;
 }
 
-// // // --------REMOVE FUNCTION(delet)--------
-// // function dltBook(e) {
-// //   if (e.target.classList.contains('delBtn')) {
-// //     var delIndex = Number(e.target.dataset.id);
-// //     bookList.splice(delIndex, 1);
-// //     updtStrg();
-// //     rndrBooks();
-// //   }
-// // }
-// // function hideAllBooks(e) {
-// //   if (e.target.checked == true) {
-// //     // renderBox.style.display = 'none';
-// //   } else renderBox.style.display = 'block';
-// // }
+// --------REMOVE FUNCTION(delet)--------
+function dltBook(e) {
+  if (e.target.classList.contains('delete')) {
+    var delIndex = Number(e.target.dataset.id);
+    bookList.splice(delIndex, 1);
+    updtStrg();
+    rndrBooks(htmlList);
+    srchFltr.value = "";
+  }
+}
+
+// ------HIDE ALL BOOKS-------
+function hideAllBooks(e) {
+  var hddnArr = [];
+  if (e.target.checked == false) {
+    hddnArr = htmlList;
+  }
+  rndrBooks(hddnArr);
+}
 
 // --------------SEARCH-------------
 function srchBook(e) {
-  var Li = document.querySelectorAll("ul li");
-  Li.forEach((v, i) => {
-    if (!v.innerText.toLowerCase().includes(e.target.value)) {
-      v.style.display = "none";
-    } else {
-      v.style.display = "block";
-    }
-  });
+  var filter = e.target.value.toLowerCase();
+  var fltrArr = htmlList.filter((v, i) => bookList[i].toLowerCase().includes(filter));
+  rndrBooks(fltrArr); 
 }
 
-function keyDown(event) {
-  if (event.keyCode == 13) {
-    addBook();
-  }
-}
-// // hideChkBox.addEventListener('click', hideAllBooks);
-// renderBox.addEventListener('click', dltBook);
-btn.addEventListener("click", addBook);
-input.addEventListener("keydown", keyDown);
+hideChkBox.addEventListener('click', hideAllBooks);
+renderBox.addEventListener('click', dltBook);
+btn.addEventListener("click", addBook );
+srchFltr.addEventListener('keyup',srchBook);
