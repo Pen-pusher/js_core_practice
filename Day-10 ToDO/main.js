8b6d6fcfaf6094fec99357e2baf6ac6def34262b
klassynihal <harshaan.n.khan@gmail.com>
2018-10-24 17:16:38 +0530

    modified todo

--- Day-10 ToDO/main.js
+++ Day-10 ToDO/main.js
@@ -1,67 +1,59 @@
 let btn = document.getElementById('btn');
 let inputInfo = document.getElementById('input');
 let outputbox = document.getElementById('output');
-var trueList = [];
+var toDoList = [];
 
-
+// -----------------DISPLAY FUNCTION-----------
 function renderFunc() {
   outputbox.innerHTML = '';
-  trueList.map( (v,i) =>
-    {
-    let trueObj = v;
+  toDoList.map( (v,i) => {
+    let toDoObj = v;
     let checked = v.checked;
     let renderStr ="";
     if(v.checked) {
-      renderStr = (`<input type = "checkbox" class = "check" data-id = "${i}" checked> <p data-id = "${i}" class = "checktrue"> ${trueObj.listItem}</p> <button class="btn">del</button>`)
+      renderStr = (`<input type = "checkbox" class = "check" data-id = "${i}" checked> <p data-id = "${i}" class = "checktrue"> ${toDoObj.listItem}</p> <button class="btn">del</button>`)
     } else {
-      renderStr = (`<input type = "checkbox" class = "check" data-id = "${i}"> <p data-id = "${i}" > ${trueObj.listItem}</p> <button class="btn" data-id="${i}">del</button>`)
+      renderStr = (`<input type = "checkbox" class = "check" data-id = "${i}"> <p data-id = "${i}" > ${toDoObj.listItem}</p> <button class="btn" data-id="${i}">del</button>`)
     }
     let li = document.createElement("LI");
     li.innerHTML = renderStr;
     outputbox.appendChild(li);
     return ;
-  }
-  )
+  });
 }
 
+// ------ADD TO ARRAY FUNCTION---------
 function addFunc() {
-  let trueObj = {}
+  let toDoObj = {}
   if (inputInfo.value) {
-    trueObj.listItem = inputInfo.value;
-    trueObj.checked = false;
-    trueList.push(trueObj);
+    toDoObj.listItem = inputInfo.value;
+    toDoObj.checked = false;
+    toDoList.push(toDoObj);
     renderFunc();
   }
   inputInfo.value = '';
+  return ;
 }
 
-// deletFunc() {
+// --------UPDATE FUNCTION(delet & Check)--------
+function updateFunc(e) {
+  if(e.target.classList.contains('btn') ) {
+    var delIndex = Number(e.target.dataset.id);
+    toDoList.splice(delIndex,1);
+  }
+  if(e.target.classList.contains('check') ) {
+    var checkIndex = Number(e.target.dataset.id);
+    toDoList[checkIndex].checked = e.target.checked;
+  }
+  renderFunc();
+}
 
-// }
-
-function keyd(event) {
+function keyDown(event) {
   if (event.keyCode == 13) {
     addFunc();
   }
 }
 
-function updateFunc(e) {
-  if(e.target.classList.contains('btn') ) {
-    var delIndex = Number(e.target.dataset.id);
-    trueList.splice(delIndex,1);
-  }
-  if(e.target.classList.contains('check') ) {
-    var checkIndex = Number(e.target.dataset.id);
-    trueList[checkIndex].checked = e.target.checked;
-    // console.log(e.target.checked);
-  }
-  renderFunc();
-}
-function checkFunc() {
-
-}
-
-outputbox.addEventListener('click', updateFunc)
-
+outputbox.addEventListener('click', updateFunc);
 btn.addEventListener("click", addFunc);
-inputInfo.addEventListener("keydown", keyd);
+inputInfo.addEventListener("keydown", keyDown);
